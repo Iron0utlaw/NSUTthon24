1439528d1ef29e762dbe0d6cb1196eea0c2bd47e
Iron0utlaw <hvarshney123@gmail.com>
2023-06-03 21:14:50 +0530

    React Router setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './comp/home'
 import Reg from './comp/reg'
 
 function App() {
   return (
-    <div>
-      {/* <Home/> */}
-      <Reg/>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/r' element={<Reg/>}/>
+    </Routes>
   );
 }
 
