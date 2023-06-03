45bb14a4fdca9bc5dcd58080db12bf6e063721d2
Harsh Varshney <harsh@Harshs-MacBook-Air.local>
2023-06-03 21:26:36 +0530

    Button to navigate added

--- src/comp/home.js
+++ src/comp/home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './home.css'
 import {ReactComponent as Thon} from '../assets/Thon.svg'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div className='main-container'>
         <Thon/>
+        <Link to="/r">Next</Link>
     </div>
   )
 }
