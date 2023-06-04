ef0dfedccd67648eaca14f82218956e9762037ca
Iron0utlaw <hvarshney123@gmail.com>
2023-06-04 23:46:31 +0530

    React-supabase uninstalled

--- src/comp/Team.js
+++ src/comp/Team.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './team.css'
 
 const Team = ({entry}) => {
   return (
-    <div>
-        <h1>{entry.team[0].name}</h1>
+    <div className='team-card'>
+        <h1>{entry.id}</h1>
+        <h1>{entry.team[0].teamName}</h1>
     </div>
   )
 }
