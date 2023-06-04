69dd77f5d7c33f6e30307c9f75cecbe62594f6c2
Harsh Varshney <harsh@Harshs-MacBook-Air.local>
2023-06-04 15:39:47 +0530

    Update

--- src/comp/Preview.js
+++ src/comp/Preview.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './pre.css'
 
 const Preview = ({person}) => {
+    console.log(person.leader)
   return (
     <div className='card'>
         <h2>Name = {person.name}</h2>
         <h2>Branch = {person.branch}</h2>
         <h2>Roll No = {person.rollno}</h2>
         <h2>Email = {person.email}</h2>
+        {person.leader && <h2>Leader</h2>}
     </div>
   )
 }
