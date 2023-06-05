61f771f2108cfc1ed8324221697c90b91d800c1b
Harsh Varshney <harsh@Harshs-MacBook-Air.local>
2023-06-05 10:38:27 +0530

    Search function

--- src/comp/Teams.js
+++ src/comp/Teams.js
@@ -4,6 +4,7 @@ import supabase from '../supabase'
 
 const Teams = () => {
     const [allTeams,setAllTeams] = useState([]);
+    const [search,setSearch] = useState('');
     async function fetchData(){
         let { data: thon, error } = await supabase
         .from('thon')
@@ -17,8 +18,13 @@ const Teams = () => {
   return (
     <div>
         <h1>Teams</h1>
+        <input type='text' placeholder='Search Team' onChange={(e) => setSearch(e.target.value)}></input>
         {
-            allTeams.map((entry) => {
+            allTeams.filter((entry) => {
+                return search.toLowerCase === ''
+                ? entry
+                : entry.team[0].teamName.toLowerCase().includes(search) || entry.id.toString().includes(search);
+            }).map((entry) => {
                 return <Team key={entry.id} entry={entry}/>
             })
         }
