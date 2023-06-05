c94facc26a1b885e72d600b95106890fe56879b5
Harsh Varshney <harsh@Harshs-MacBook-Air.local>
2023-06-05 22:36:25 +0530

    error handling

--- src/comp/Teams.js
+++ src/comp/Teams.js
@@ -5,19 +5,29 @@ import supabase from '../supabase'
 const Teams = () => {
     const [allTeams,setAllTeams] = useState([]);
     const [search,setSearch] = useState('');
+    const [loading,setLoading] = useState(false);
+    const [err,setErr] = useState(false);
     async function fetchData(){
+        setLoading(true)
         let { data: thon, error } = await supabase
         .from('thon')
         .select('*')
         setAllTeams(thon);
+
+        if (error) {
+            setErr(true);
+        }
+        setLoading(false)
     }
 
     useEffect(() => {
         fetchData();
     },[])
+
+    if(err) return <h1>ERROR</h1>
   return (
     <div>
-        <h1>Teams</h1>
+        {loading ? <h1>Loading</h1> : <h1>Teams</h1>}
         <input type='text' placeholder='Search Team' onChange={(e) => setSearch(e.target.value)}></input>
         <h2>{allTeams.length}</h2>
         {
