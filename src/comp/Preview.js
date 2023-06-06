b562955d25ccc7c47ebe89e894164104289bb19f
Iron0utlaw <hvarshney123@gmail.com>
2023-06-06 13:59:04 +0530

    Modal CSS added part 1

--- src/comp/Preview.js
+++ src/comp/Preview.js
@@ -8,7 +8,7 @@ const Preview = ({person}) => {
         <h2>Branch = {person.branch}</h2>
         <h2>Roll No = {person.rollno}</h2>
         <h2>Email = {person.email}</h2>
-        {person.leader && <h2>Leader</h2>}
+        {/* {person.leader && <h2>Leader</h2>} */}
     </div>
   )
 }
