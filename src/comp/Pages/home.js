1a2ae91e997c999b6ef25d9a054f7c9bd1052ae4
Gaurav Tiwary <97665755+Gaurav-1306@users.noreply.github.com>
2023-06-09 10:52:38 +0530

    button add

--- src/comp/Pages/home.js
+++ src/comp/Pages/home.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import './home.css'
-import {ReactComponent as Thon} from '../../assets/Thon.svg'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./home.css";
+import { ReactComponent as Thon } from "../../assets/Thon.svg";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className='main-container'>
-        <Thon className='main-logo'/>
-        <div className='link-container'>
-          <Link className='link' to="/registration">Register</Link>
-          <Link className='link' to="/teams">Teams</Link>
+    <div className="main-container">
+      <Thon className="main-logo" />
+      <div className="link-container">
+        <div class="container">
+          <button class="button">New user</button>
         </div>
+        <Link className="link" to="/registration">
+          Register
+        </Link>
+        <Link className="link" to="/teams">
+          Teams
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
